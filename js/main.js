c7488eff1c51cc4574852d8c91978932ceb0c24e
Shawn Busolits <api.sbusolits@gmail.com>
2015-11-18 15:22:39 -0500

    Removed showPaths checkbox - it's too fast now

--- js/main.js
+++ js/main.js
@@ -7,16 +7,7 @@ var Application = function() {
 }
 
 Application.prototype.getToIt = function() {
-  /*var showPaths = document.getElementById('show-paths').checked;
-  if (showPaths) {
-    alert("You have opted to show paths in real time.\n" +
-          "This can be super slow!\n"+
-          "When the next alert box appears, click\n" +
-          "\"Prevent this page from creating additional dialogs\"\n"+
-          "(or similar)");
-    alert("Click the checkbox below, then click \"OK\"");
-  }*/
-  this.game = new Game(/*showPaths*/);
+  this.game = new Game();
   this.game.buildBoard(document.getElementById("grid").value);
   // allTheWords is set by trie.json
   this.allTheWords = allTheWords;
