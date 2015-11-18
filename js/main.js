cd6a950cef6cbd62cc254985849f8fe89a976101
Shawn Busolits <api.sbusolits@gmail.com>
2015-11-18 13:04:57 -0500

    Re-objectified Application

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
-var consoleDiv = document.getElementById('console');
-var colors = ["red", "yellow", "green", "blue"];
-var colorIndex = 0;
-var knownSolutions = [];
+var Application = function() {
+  this.consoleDiv = document.getElementById('console');
+  this.colors = ["red", "yellow", "green", "blue"];
+  this.colorIndex = 0;
+  this.knownSolutions = [];
+  this.getToIt();
+}
 
-function getToIt() {
+Application.prototype.getToIt = function() {
   var showPaths = document.getElementById('show-paths').checked;
   if (showPaths) {
     alert("You have opted to show paths in real time.\n" +
@@ -16,24 +19,23 @@ function getToIt() {
   var game = new Game(showPaths);
   game.buildBoard(document.getElementById("grid").value);
   // allTheWords is set by trie.json
-  setNodeProtos(allTheWords);
-  setTimeout(function() {
-    for (var i=0; i<game.getHeight(); i++) {
-      for (var j=0; j<game.getWidth(); j++) {
-        walkBoard(game, '', game.wordLengths, i, j, allTheWords, [], []);
-      }
+  this.allTheWords = allTheWords;
+  this.setNodeProtos(this.allTheWords);
+  for (var i=0; i<game.getHeight(); i++) {
+    for (var j=0; j<game.getWidth(); j++) {
+      this.walkBoard(game, '', game.wordLengths, i, j, this.allTheWords, [], []);
     }
-  }, 50);
-}
-
-function setNodeProtos(node) {
-  node.__proto__ = Node.prototype;
-  for (child in node.children) {
-    setNodeProtos(node.children[child]);
   }
 }
 
-function trieHasWord(node, word, isFullWord) {
+Application.prototype.setNodeProtos = function(node) {
+  node.__proto__ = Node.prototype;
+  for (child in node.children) {
+    this.setNodeProtos(node.children[child]);
+  }
+}
+
+Application.prototype.trieHasWord = function(node, word, isFullWord) {
   if (word.length == 0) {
     if (isFullWord) {
       if (node.endWord) {
@@ -48,43 +50,43 @@ function trieHasWord(node, word, isFullWord) {
   var letterToFind = word.charAt(0);
   var childWithLetter = node.getChildWithValue(letterToFind);
   if (childWithLetter) {
-    return trieHasWord(childWithLetter, word.substring(1), isFullWord);
+    return this.trieHasWord(childWithLetter, word.substring(1), isFullWord);
   } else {
     return false;
   }
 }
 
-function walkBoard(game, word, wordLengths, row, col, trie, words, nodesInWord) {
+Application.prototype.walkBoard = function(game, word, wordLengths, row, col, trie, words, nodesInWord) {
   game.board[row][col].setVisited(true);
   game.drawBoard();
   word = word + game.board[row][col].letter;
   nodesInWord.push(game.board[row][col]);
-  if (!trieHasWord(trie, word, false) ||
+  if (!this.trieHasWord(trie, word, false) ||
       word.length > Math.max.apply(null, wordLengths)) {
     game.board[row][col].setVisited(false);
     game.drawBoard();
-    removeValueFromArray(game.board[row][col], nodesInWord);
+    this.removeValueFromArray(game.board[row][col], nodesInWord);
     return;
   }
-  if (trieHasWord(trie, word, true) &&
+  if (this.trieHasWord(trie, word, true) &&
       wordLengths.indexOf(word.length) !== -1) {
     words.push(word);
-    game.setInWords(nodesInWord, colors[colorIndex++]);
-    removeValueFromArray(word.length, wordLengths);
+    game.setInWords(nodesInWord, this.colors[this.colorIndex++]);
+    this.removeValueFromArray(word.length, wordLengths);
     if (wordLengths.length == 0) {
-      if (!knownSolutionExists(words.sort())) {
+      if (!this.knownSolutionExists(words.sort())) {
         game.printAnswer(words);
         var wordsCopy = JSON.parse(JSON.stringify(words)).sort();
-        knownSolutions.push(wordsCopy);
+        this.knownSolutions.push(wordsCopy);
       }
       game.clearInWords(nodesInWord);
-      colorIndex--;
+      this.colorIndex--;
       game.board[row][col].setVisited(false);
       game.drawBoard();
-      removeValueFromArray(game.board[row][col], nodesInWord);
-      removeValueFromArray(word, words);
+      this.removeValueFromArray(game.board[row][col], nodesInWord);
+      this.removeValueFromArray(word, words);
       if (words.length == 0) {
-        colorIndex = 0;
+        this.colorIndex = 0;
       }
       wordLengths.push(word.length);
       return;
@@ -92,15 +94,15 @@ function walkBoard(game, word, wordLengths, row, col, trie, words, nodesInWord) 
     for (var i=0; i<game.getHeight(); i++) {
       for (var j=0; j<game.getWidth(); j++) {
         if (!(i == row && j == col) &&
-            validCell(game, i, j) &&
+            this.validCell(game, i, j) &&
             !game.board[i][j].visited) {
-          setTimeout(walkBoard(game, "", wordLengths, i, j, trie, words, []), 50);
+          this.walkBoard(game, "", wordLengths, i, j, trie, words, []);
         }
       }
     }
-    removeValueFromArray(word, words);
+    this.removeValueFromArray(word, words);
     game.clearInWords(nodesInWord);
-    colorIndex--;
+    this.colorIndex--;
     wordLengths.push(word.length);
   }
   for (var i=-1; i<2; i++) {
@@ -108,34 +110,34 @@ function walkBoard(game, word, wordLengths, row, col, trie, words, nodesInWord) 
       var newRow = row + i;
       var newCol = col + j;
       if (!(newRow == row && newCol == col) &&
-          validCell(game, newRow, newCol) &&
+          this.validCell(game, newRow, newCol) &&
           !game.board[newRow][newCol].visited) {
-        setTimeout(walkBoard(game, word, wordLengths, newRow, newCol, trie, words, nodesInWord), 50);
+        this.walkBoard(game, word, wordLengths, newRow, newCol, trie, words, nodesInWord);
       }
     }
   }
   game.board[row][col].setVisited(false);
   game.drawBoard();
-  removeValueFromArray(game.board[row][col], nodesInWord);
+  this.removeValueFromArray(game.board[row][col], nodesInWord);
 }
 
-function validCell(game, row, col) {
+Application.prototype.validCell = function(game, row, col) {
   return (row >= 0 && row < game.getHeight() &&
           col >= 0 && col < game.getWidth());
 }
 
-function removeValueFromArray(value, array) {
+Application.prototype.removeValueFromArray = function(value, array) {
   var i = array.indexOf(value);
   if (i !== -1) {
     array.splice(i, 1);
   }
 }
 
-function knownSolutionExists(words) {
+Application.prototype.knownSolutionExists = function(words) {
   // Return words in knownSolutions
-  var i, j;
-  for (i in knownSolutions) {
-    currentTest = knownSolutions[i];
+  var i, j, currentTest;
+  for (i in this.knownSolutions) {
+    currentTest = this.knownSolutions[i];
     for (j in words) {
       if (currentTest[j] != words[j]) {
         break;
